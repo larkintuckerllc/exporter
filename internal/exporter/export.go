0a3a9795638b4db93bfcc78ccc3eb54371f28370
John Tucker <jtucker@spotify.com>
2021-01-23 14:36:55 -0700

    time logic

--- internal/exporter/export.go
+++ internal/exporter/export.go
@@ -3,13 +3,17 @@ package exporter
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 func export(start int, end int, minimum int, count int) {
-	// TODO: USE TIME
-	m := float64(minimum)
-	c := float64(count)
-	value := int(math.Max(math.Ceil((m/c)*100), 100))
+	hour := time.Now().UTC().Hour()
+	value := 100
+	if (start < end && hour >= start && hour <= end) || (start > end && (hour >= start || hour <= end)) {
+		m := float64(minimum)
+		c := float64(count)
+		value = int(math.Max(math.Ceil((m/c)*100), 100))
+	}
 	// TODO: SEND TO CLOUD MONITORING
 	fmt.Println(value)
 }
