bc0898d48f18e1185f96dca62b96457374225c9c
John Tucker <jtucker@spotify.com>
2021-01-23 11:11:42 -0700

    initial count implementation

--- internal/exporter/initial_count.go
+++ internal/exporter/initial_count.go
@@ -1,9 +1,26 @@
 package exporter
 
 import (
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	coreV1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
 func initialCount(api coreV1.EndpointsInterface, service string) (int, string, error) {
-	return 0, "", nil
+	listOptions := metaV1.ListOptions{}
+	endpointsList, err := api.List(listOptions)
+	if err != nil {
+		return 0, "", err
+	}
+	count := 0
+	resourceVersion := endpointsList.ListMeta.ResourceVersion
+	for _, endpoints := range endpointsList.Items {
+		name := endpoints.ObjectMeta.Name
+		if name != service {
+			continue
+		}
+		for _, endpoint := range endpoints.Subsets {
+			count += len(endpoint.Addresses)
+		}
+	}
+	return count, resourceVersion, nil
 }
