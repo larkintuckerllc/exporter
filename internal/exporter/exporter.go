719134d08cf978061fe48357eb980631a9e7ab62
John Tucker <jtucker@spotify.com>
2021-01-23 09:05:16 -0700

    ticker

--- internal/exporter/exporter.go
+++ internal/exporter/exporter.go
@@ -1,9 +1,27 @@
 package exporter
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func Execute(namespace string, service string) error {
 	fmt.Println(namespace)
 	fmt.Println(service)
+	done := make(chan bool)
+	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-done:
+				ticker.Stop()
+				return
+			case <-ticker.C:
+				fmt.Println("Hello !!")
+			}
+		}
+	}()
+	time.Sleep(10 * time.Second)
+	done <- true
 	return nil
 }
