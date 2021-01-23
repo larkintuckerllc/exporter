047ac8c954bf7be276b44030b86ce4fcf3ca5291
John Tucker <jtucker@spotify.com>
2021-01-23 15:48:30 -0700

    fix value logic

--- internal/exporter/export.go
+++ internal/exporter/export.go
@@ -8,11 +8,11 @@ import (
 
 func export(start int, end int, minimum int, count int) {
 	hour := time.Now().UTC().Hour()
-	value := 100
+	value := 0
 	if (start < end && hour >= start && hour <= end) || (start > end && (hour >= start || hour <= end)) {
 		m := float64(minimum)
 		c := float64(count)
-		value = int(math.Max(math.Ceil((m/c)*100), 100))
+		value = int(math.Ceil((m / c) * 100))
 	}
 	// TODO: SEND TO CLOUD MONITORING
 	fmt.Println(value)
